bddf2eb843e8488520b949cde724f348a52581ac
MoshMage <moshmage@gmail.com>
2022-08-11 09:19:49 +0100

    repeated imports -.-'

--- contracts/bepro/NetworkRegistry.sol
+++ contracts/bepro/NetworkRegistry.sol
@@ -5,7 +5,6 @@ import "@openzeppelin/contracts/math/SafeMath.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/utils/EnumerableSet.sol";
 import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
-import "@openzeppelin/contracts/math/SafeMath.sol";
 
 import "./INetworkV2.sol";
 import "../utils/Governed.sol";
--- contracts/bepro/NetworkV2.sol
+++ contracts/bepro/NetworkV2.sol
@@ -3,7 +3,6 @@ pragma experimental ABIEncoderV2;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
-import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/math/SafeMath.sol";
 
 import "../utils/Governed.sol";
