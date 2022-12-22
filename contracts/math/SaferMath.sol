395fe2a96b39769a72141d91f469afd91bbb34e7
MoshMage <moshmage@gmail.com>
2022-12-22 06:57:49 +0000

    imports erc4626 and introduces new folder structure

---
+++ contracts/math/SaferMath.sol
@@ -0,0 +1,27 @@
+pragma solidity >=0.6.0 <0.8.0;
+
+library SaferMath {
+    enum Rounding {
+        Down, // Toward negative infinity
+        Up, // Toward infinity
+        Zero // Toward zero
+    }
+
+    /**
+     * @notice Calculates x * y / denominator with full precision, following the selected rounding direction.
+     */
+    function mulDiv(
+        uint256 x,
+        uint256 y,
+        uint256 denominator,
+        Rounding rounding
+    ) internal pure returns (uint256) {
+        uint256 result = mulDiv(x, y, denominator, rounding);
+        if (rounding == Rounding.Up && mulmod(x, y, denominator) > 0) {
+            result += 1;
+        }
+        return result;
+    }
+
+
+}
