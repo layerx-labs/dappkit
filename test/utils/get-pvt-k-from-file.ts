55214cee9b4ccf32bb6468a8659d1ca239915801
MoshMage <moshmage@gmail.com>
2023-09-04 09:41:34 +0100

    remove file from yml and change getPrivateKey to work with mnemonic

--- test/utils/get-pvt-k-from-file.ts
+++ test/utils/get-pvt-k-from-file.ts
@@ -1,10 +1,16 @@
-import {readFileSync} from "fs";
+import {readFileSync, existsSync} from "fs";
 import {resolve} from "path";
+import {config} from "hardhat";
+import {deriveKeyFromMnemonicAndPath} from "hardhat/internal/util/keys-derivation";
+import {type HardhatNetworkHDAccountsConfig} from "hardhat/src/types/config";
 
 /**
  * Returns the private key from the output provided by ganache
  * @param index
  */
 export function getPrivateKeyFromFile(index = 0) {
-  return Object.values(JSON.parse(readFileSync(resolve('./keys.json'), 'utf-8')).private_keys)[index] as string;
+  if (existsSync(resolve('./keys.json')))
+    return Object.values(JSON.parse(readFileSync(resolve('./keys.json'), 'utf-8')).private_keys)[index] as string;
+  const accounts = config.networks.hardhat.accounts as HardhatNetworkHDAccountsConfig;
+  return deriveKeyFromMnemonicAndPath(accounts.mnemonic, accounts.path + `/${index}`, ``);
 }
